d90e649a5b97169e253d7d9f294ef273587549ca
BrianHKanis <brianhkanis@gmail.com>
2023-08-14 10:36:21 -0500

    Most recent

--- etl/load_export/src/postgres_export/log_data_quality.py
+++ etl/load_export/src/postgres_export/log_data_quality.py
@@ -1,31 +1,88 @@
 import json
 import pandas as pd
-
+from ...data.data_type_columns import data_type_dictionary
 # from console import merged
 # table_data = merged
 
 with open('whatcom.json', 'r') as f: #For quicker worflow rather than importing
     table_data = json.load(f)
 
-# CHECK ---> '\\xa0'
-
 topic_names = ['organization', 'location', 'service', 'service_at_location',
         'contact', 'schedule', 'phone', 'taxonomy', 'taxonomy_term']
 
-poor_data = {'spaces': [], 'non_breaking_space': [],
-             'width_space': [], 'newline': [],
-             'double_quotes': [], 'non_castable': [],
-             'semi_colons': [], 'single_quotes': []}
+poor_data = {'non_castable': []}
 
-
-
-def append_to_log(defect_type, table_name, record, v):
+def append_to_log(defect_type, table_name, record, v, k=''):
     if 'source_id' in record.keys():
-        data_dict = {'table_name': table_name, 'id': record['id'], 'source_id': record['source_id'], 'value': v}
+        data_dict = {'table_name': table_name, 'id': record['id'], 'source_id': record['source_id'], 'column': k, 'value': v}
         poor_data[defect_type].append(data_dict)
     else:
-        data_dict = {'table_name': table_name, 'id': record['id'], 'value': v}
+        data_dict = {'table_name': table_name, 'id': record['id'], 'column': k, 'value': v}
         poor_data[defect_type].append(data_dict)
+
+def find_cannot_be_casted(table_name):
+    table = table_data[table_name]
+    data_type_table = data_type_dictionary[table_name]
+    for record in table:
+        for k, v in record.items():
+            data_type = data_type_table[k]
+            try:
+                v = data_type(v)
+            except:
+                append_to_log('non_castable', table_name, record, v, k=k)
+
+
+def find_all_cannot_be_casted():
+    for table in topic_names:
+        find_cannot_be_casted(table)
+    with open("z_quality_check.json", "w") as final:
+        json.dump(poor_data, final, indent=4)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+poor_data = {'non_castable': [], 'spaces': [],
+             'non_breaking_space': [],
+             'width_space': [], 'newline': [],
+             'double_quotes': [], 'semi_colons': [],
+             'single_quotes': []}
 
 def find_spaces(table_name):
     table = table_data[table_name]
@@ -69,13 +126,6 @@ def find_double_quotes(table_name):
                 if '"' in v:
                     append_to_log('double_quotes', table_name, record, v)
 
-def find_cannot_be_casted(table_name):
-    table = table_data[table_name]
-    for record in table:
-        if 'interval' in record.keys():
-            v = record['interval']
-            if type(v) != int:
-                append_to_log('non_castable', table_name, record, v)
 
 def find_semi_colons(table_name):
     table = table_data[table_name]
@@ -92,6 +142,7 @@ def find_single_quotes_chars(table_name):
             if type(v) == str:
                 if '\u2019' in v:
                     append_to_log('single_quotes', table_name, record, v)
+
 #########################
 def list_all_columns(table_name):
     table = table_data[table_name]
@@ -114,22 +165,15 @@ def print_column_and_type(table_name, column_name):
         if column_name in record.keys():
             print(record[column_name], type(record[column_name]))
 
-def find_all_per_table(table_name):
-    find_spaces(table_name)
-    find_non_breaking_space(table_name)
-    find_width_space(table_name)
-    find_newline(table_name)
-    find_cannot_be_casted(table_name)
-    find_double_quotes(table_name)
-    find_semi_colons(table_name)
-    find_single_quotes_chars(table_name)
+# def find_all_per_table(table_name):
+#     find_cannot_be_casted(table_name)
+    # find_spaces(table_name)
+    # find_non_breaking_space(table_name)
+    # find_width_space(table_name)
+    # find_newline(table_name)
+    # find_double_quotes(table_name)
+    # find_semi_colons(table_name)
+    # find_single_quotes_chars(table_name)
     
-def find_all():
-    for table in topic_names:
-        find_all_per_table(table)
 
 
-find_all()
-
-with open("z_quality_check.json", "w") as final:
-   json.dump(poor_data, final, indent=4)
